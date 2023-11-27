e7c6feca42893c2bd2f33ce14b1b9b672dab4bac
Mohamed-Amine-Fki <147945209+Mohamed-Amine-Fki@users.noreply.github.com>
2023-11-27 22:48:36 +0100

    Add files via upload

---
+++ tp5/Fkimohamedamine_LSI3_DAR_TP_5_2_RMIBanque_Server-master/src/rmiServer/BanqueServer.java
@@ -0,0 +1,19 @@
+package rmiServer;
+
+import rmiService.BanqueImpl;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class BanqueServer {
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099); //anniaire
+            BanqueImpl BI = new BanqueImpl(); // Skeleton
+            System.out.println(BI.toString());
+            Naming.rebind("rmi://localhost:1099/BanqueObject",BI);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
